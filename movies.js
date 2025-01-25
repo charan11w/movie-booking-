933eb49e2673fa134d3e638f1330c688b47d491e
S Charan Kumar <charan11w@gmail.com>
2025-01-25 13:51:43 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,21 @@
+const cinema = {
+  name: 'Triveni',
+  location: 'ananthapur',
+  movies: [{
+    title: 'daku maharaj',
+    shows: ['11:00PM', '2:30PM'],
+    seats: [40, 20]
+  },
+  {
+    title: 'Intersteller',
+    shows: ['11:00PM', '2:30PM'],
+    seats: [42, 20]
+  },
+  {
+    title: 'daku maharaj',
+    shows: ['11:00PM', '2:30PM'],
+    seats: [10, 20]
+  }
+  ]
+}
+
